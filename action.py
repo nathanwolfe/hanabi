ba73c161f0dae981d43458fe8e27eb15793c67c3
Nathan Wolfe <nathanwolfeRL@gmail.com>
2014-07-23 21:22:55 -0700

    added action class

---
+++ action.py
@@ -0,0 +1,8 @@
+# when the interface requests a move from the player, the player returns an Action
+
+class Action:
+    def __init__(self, t, c, p, i):
+        self.type = t   # action type as string ("play", "discard", or "hint")
+        self.card = c   # list of card positions in hand ([0] through [4], or list with multiple for hint)
+        self.info = i   # card information (color string or number for hint, otherwise None)
+        self.player = p # hint recipient, if not hint None
