c36e776367334ee5c485d91a05643ae790f74fdb
Nathan Wolfe <nathanwolferl@gmail.com>
2014-08-01 16:58:23 -0700

    implemented rearrangement

--- player.py
+++ player.py
@@ -25,6 +25,7 @@ class Player:
             if self.oldstate.curplayer == self.number:
                 del(self.hplayable[self.state.action.cards])
                 self.hplayable.append(False)
+                self.drew = True
             else:
                 curcard = self.oldstate.hands[self.oldstate.curplayer].cards[self.state.action.cards]
                 if curcard in self.xplayable:
@@ -36,7 +37,23 @@ class Player:
             if i != self.number:
                 for j in range(5):
                     if -1 not in self.state.hands[i].info[j] and self.state.hands[i].cards[j] not in self.xplayable:
-                        self.xplayable.append(self.state.hands[i].cards[j]
+                        self.xplayable.append(self.state.hands[i].cards[j])
+        if self.drew:
+            del(self.hplayable(4))
+        hasinfo = [[False, i] for i in range(len(self.hplayable))]
+        for i in range(len(hasinfo)):
+            if self.hplayable[i] or self.state.hands[self.number].info[i] != [-1, -1]:
+                hasinfo[i][0] = True
+        hasinfo.sort(reverse=True, key=lambda card: card[0])
+        if self.drew:
+            if False in zip(*hasinfo)[0]: # if the set of 0th elements of hasinfo has False in it
+                hasinfo.insert(zip(*hasinfo)[0].index(False), [False, 4])
+            else:
+                hasinfo.append([False, 4])
+            self.hplayable.append(False)
+        temphplayable = [self.hplayable[hasinfo[i][1]] for i in range(5)]
+        self.hplayable = temphplayable
+        return [hasinfo[i][1] for i in range(5)]
 
     def scan(self, newstate):
         self.state = newstate
