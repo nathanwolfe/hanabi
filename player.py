f283da859f3aa89d481282bfc729907cf654d03c
Nathan Wolfe <nathanwolferl@gmail.com>
2014-08-01 16:12:03 -0700

    finished playable card management

--- player.py
+++ player.py
@@ -29,6 +29,14 @@ class Player:
                 curcard = self.oldstate.hands[self.oldstate.curplayer].cards[self.state.action.cards]
                 if curcard in self.xplayable:
                     del(self.xplayable[self.xplayable.index(curcard)])
+        for i in range(5):
+            if -1 not in self.state.hands[self.number].info[i]:
+                self.hplayable[i] = True
+        for i in range(len(self.state.hands)):
+            if i != self.number:
+                for j in range(5):
+                    if -1 not in self.state.hands[i].info[j] and self.state.hands[i].cards[j] not in self.xplayable:
+                        self.xplayable.append(self.state.hands[i].cards[j]
 
     def scan(self, newstate):
         self.state = newstate
