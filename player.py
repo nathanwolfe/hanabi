aa139a06f1155bdb946f87760e18b207a1b10944
Nathan Wolfe <nathanwolferl@gmail.com>
2014-08-05 10:55:15 -0700

    player now works with any hand size, no longer values random info

--- player.py
+++ player.py
@@ -19,6 +19,7 @@ class Player:
     def rearrange(self, newstate):
         self.oldstate = self.state
         self.state = newstate
+        handsize = len(self.state.hands[self.number].info)
         if self.state.action.type in ["number", "color"] and len(self.state.action.cards) == 1:
             if self.state.action.player == self.number:
                 self.hplayable[self.state.action.cards[0]] = True
@@ -33,30 +34,30 @@ class Player:
                 curcard = self.oldstate.hands[self.oldstate.curplayer].cards[self.state.action.cards]
                 if curcard in self.xplayable:
                     del(self.xplayable[self.xplayable.index(curcard)])
-        for i in range(5):
+        for i in range(handsize):
             if -1 not in self.state.hands[self.number].info[i]:
                 self.hplayable[i] = True
         for i in range(len(self.state.hands)):
             if i != self.number:
-                for j in range(5):
+                for j in range(len(self.state.hands[i].cards)):
                     if -1 not in self.state.hands[i].info[j] and self.state.hands[i].cards[j] not in self.xplayable:
                         self.xplayable.append(self.state.hands[i].cards[j])
         if self.drew:
-            del(self.hplayable(4))
+            del(self.hplayable(handsize-1))
         hasinfo = [[False, i] for i in range(len(self.hplayable))]
         for i in range(len(hasinfo)):
-            if self.hplayable[i] or self.state.hands[self.number].info[i] != [-1, -1]:
+            if self.hplayable[i] #or self.state.hands[self.number].info[i] != [-1, -1]:
                 hasinfo[i][0] = True
         hasinfo.sort(reverse=True, key=lambda card: card[0])
         if self.drew:
             if False in zip(*hasinfo)[0]: # if the set of 0th elements of hasinfo has False in it
-                hasinfo.insert(zip(*hasinfo)[0].index(False), [False, 4])
+                hasinfo.insert(zip(*hasinfo)[0].index(False), [False, handsize-1])
             else:
-                hasinfo.append([False, 4])
+                hasinfo.append([False, handsize-1])
             self.hplayable.append(False)
-        temphplayable = [self.hplayable[hasinfo[i][1]] for i in range(5)]
+        temphplayable = [self.hplayable[hasinfo[i][1]] for i in range(handsize)]
         self.hplayable = temphplayable
-        return [hasinfo[i][1] for i in range(5)]
+        return [hasinfo[i][1] for i in range(handsize)]
 
     def scan(self, newstate):
         self.state = newstate
